5c2e03fca455ab136e78b96a58531a32a087019f
Daniel Risdon <drisdon07@gmail.com>
2017-09-14 10:29:06 -0400

    add combat functionality

--- README.md
+++ README.md
@@ -16,3 +16,16 @@ You start outside the portal into another dimension. You are presented with 2 ch
 Buying a new laptop ends the game immediately. Going into the portal initiates the game. You will progress through a series of story choices,
 some involving dialogue, some involving combat. Wrong choices in combat might lead to you taking damage. You can make choices along the way that
 give you more health, or new weapons and armor. If you make it to the final story scene and defeat the final boss without losing all health, you win!
+
+*combat:*
+When you make a choice that has you fighting (they will say [combat] next to them), your damage will be compared to the enemy's health. If you can't defeat them in 1 hit, you will take damage for every hit it takes you to win. If your health hits 0, you lose. 
+
+**DOCUMENTATION**
+
+This game is designed to take any story input you may desire, as long as it fits the template.
+
+The scenes array must be an array of scenes, each formatted like this:
+
+`['scene name', 'scene prompt', 'background-image', [['choice 1 prompt', 'choice 1 next scene', [enemy health, enemy damage]]]`
+
+The array of choices can have any number of choices, and the combat stats are optional.
--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
 
-  var scenes = [['start', 'WALUIGI HAS NO MONEY! WHAT SHOULD HE DO?!?!', '', [['GO GET SOME MONEY FROM WARIO!', 'new-test'], ['BE SAD!', 'newer-test']]],
-                ['new-test', 'WOW! WALUIGI NOW HAS MONEY!', '', []],
-                ['newer-test', 'YOU LOSE!', '', []]
+  var scenes = [['start', 'WALUIGI IS HUNGRY! WHAT SHOULD HE DO?!?!', '', [['GO GET SOME GROCERIES!', 'groceries'], ['GO OUT TO EAT!', 'eat-out']]],
+                ['groceries', 'WALUIGI DOESN\'T HAVE ENOUGH MONEY! WHAT SHOULD HE DO?!?!', '', [['GO GET SOME MONEY FROM WARIO!', 'win'], ['BE SAD!', 'lose']]],
+                ['eat-out', 'WALUIGI HAS TO BATTLE THE WAITER! WHAT DO?!?!', '', [['FIGHT HIM!', 'win', [11,1]],
+                  ['RUN AWAY AND BUY GROCERIES!', 'groceries'], ['BE SAD!', 'lose']]],
+                ['win', 'WOW! WALUIGI ATE AND ISN\'T HUNGRY ANYMORE!', '', [['START OVER', 'start']]],
+                ['lose', 'YOU LOSE!', '', [['START OVER', 'start']]]
               ]; // array of scene arrays
               // each array = [name, prompt, background, [array of choices]]
               // each choice = [text, next scene]
@@ -12,13 +15,41 @@ $(document).ready(function() {
   var thisScene; //current scene
 
   var player = { // player object - not in use yet
+    maxHealth: 10,
     health: 10,
+    damage: 1,
     image: './waluigi.png',
     render: function() {
       var $player = $('<div>');
       $player.addClass('player');
-      $player.css('background-image', 'url('+player.image+')');
+      $player.css('background-image', 'url('+this.image+')');
       $('#player-image').append($player);
+    },
+    fight: function(combat) { // get enemy health and damage and calculate combat result
+      enemyHealth = combat[0];
+      enemyDamage = combat[1];
+      console.log('enemy health: ' + enemyHealth + ' enemy damage: ' + enemyDamage);
+      if (this.damage >= enemyHealth) { // enemy is killed in 1 turn
+        console.log('You win in 1 hit! health remaining: ' +this.health + '/' + this.maxHealth);
+        return;
+      }
+      else {
+        while (enemyHealth > this.damage) { // take turns damaging each other
+          enemyHealth -= this.damage; //deal damage
+          this.health -= enemyDamage; // take damage
+        }
+      }
+      if (player.health <= 0) {
+        player.health = 0;
+        console.log('You fought hard, but couldn\'t win! YOU DIED!');
+      }
+      else console.log('It was a tough fight! health remaining: ' +this.health + '/' + this.maxHealth);
+    },
+    reset: function() {
+      this.maxHealth = 10;
+      this.health = 10;
+      this.damage = 1;
+      this.image = './waluigi.png';
     }
   };
 
@@ -32,25 +63,20 @@ $(document).ready(function() {
         $('#player-image').empty();
         $('#scene-text').empty();
         $('#choices').empty();
-        console.log(this.background);
+//        console.log(this.background);
         $('#container').css('background-image', 'url('+this.background+')');
         var $sceneText = $('<div>');
         $sceneText.addClass('scene');
         $sceneText.text(this.prompt);
         $('#scene-text').append($sceneText); // render scene text
         for (var i in this.choices) { // render choice text
-          var $choice = $('<div>');
-          $choice.addClass('choice');
-          $choice.addClass(this.choices[i].next)
-          $choice.text(this.choices[i].choiceText);
-          $choice.click(nextScene); // add choice event listeners
-          $('#choices').append($choice);
+          this.choices[i].render();
         }
         player.render();
       }
     }
     addChoices(scene, newScene); // add choices to choices array property
-    console.log(newScene);
+  //  console.log(newScene);
     return newScene; // return
   }
 
@@ -59,9 +85,19 @@ $(document).ready(function() {
       for (var i in scene[3]) {
         var newChoice = {
           choiceText: scene[3][i][0],
-          next: scene[3][i][1]
+          next: scene[3][i][1],
+          render: function() {
+            var $choice = $('<div>');
+            $choice.addClass('choice');
+            $choice.addClass(this.next);
+            $choice.text(this.choiceText);
+            $choice.click(nextScene); // add choice event listeners
+            $('#choices').append($choice);
+          }
         }
-        console.log(newChoice.next);
+        if (scene[3][i].length > 2 ) { // if there is an enemy in this scene
+          newChoice.combat = scene[3][i][2]; // set combat stats
+        }
         currentScene.choices.push(newChoice)
       }
     }
@@ -69,15 +105,26 @@ $(document).ready(function() {
 
   function nextScene(choice) { // when a choice is clicked
     console.log('CHOICE MADE!');
-    console.log(choice);
-    console.log(choice.target.classList[1]);
-    for (var i in sceneObjects) {
-      // if a scene in sceneObjects has the same name as the choice's next property, change to that scene
-      if (sceneObjects[i].name === choice.target.classList[1]) {
-        thisScene = sceneObjects[i]
-        sceneObjects[i].render();
-      }
+    var chosen = thisScene.choices.find(function(option) { // find choice that was clicked
+      return option.choiceText === choice.target.innerText;
+    })
+    console.log(chosen);
+    if (chosen.combat) { // combat if there is any
+      player.fight(chosen.combat);
     }
+    if (player.health === 0) {
+      thisScene = sceneObjects.find(function(scene) { // if the player is dead
+        return scene.name === 'lose';
+      })
+    }
+    else thisScene = sceneObjects.find(function(scene) { // find the scene that choice links to
+      return scene.name === chosen.next
+    })
+    console.log(thisScene.name);
+    if (thisScene.name === 'start' || thisScene.name === 'lose') { // if the game is over
+      player.reset();
+    }
+    thisScene.render();
   }
 
   for (var i in scenes) { // just for testing. main game isn't done yet, although this functions fairly well
